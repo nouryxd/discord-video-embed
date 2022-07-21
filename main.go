1f0fdaacf97577b9d04d129dc8fa6d29cecd8681
lyx0 <66651385+lyx0@users.noreply.github.com>
2022-07-21 23:56:11 +0200

    embed video, link to video on main page

--- main.go
+++ main.go
@@ -9,24 +9,71 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome!\n")
+var host = "https://dc.noury.ee"
+
+func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var videoUrl = "https://cdn.discordapp.com/attachments/381520882608373761/989666371178754068/denkcats_1639474686233272.mp4"
+
+	var indexHtml = fmt.Sprintf(`
+<html>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Discord Video Embedder</title>
+        <meta charset="UTF-8" />
+        <meta name="description" content="" />
+    </head>
+    <body style="background-color:#181a1b;">
+    <h1 style="color:#d8d4cf">Discord Video Embedder</h1>
+    <p style="color:#d8d4cf">Lets you watch a discord video in the browser instead of downloading it by default. <br>
+    Simply paste the link to a discord video at the end of the url (after the /, if there is none add one).
+    </p>
+    <p style="color:#d8d4cf">Original: <a style="color:#3391ff" href="%s/%s">%s/%s</a></p>
+    <p style="color:#d8d4cf"><sup>this is beautiful web design shut up</sup></p>
+    </body>
+</html>
+`, host, videoUrl, host, videoUrl)
+
+	fmt.Fprintf(w, "%s", indexHtml)
+	// fmt.Fprint(w, indexHtml)
 }
 
 func Video(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	if ps.ByName("video") == "/" {
-		Index(w, r)
-		// fmt.Fprintf(w, "XDDD hello")
+	if ps.ByName("video") == "/" || ps.ByName("video") == "" {
+		Index(w, r, ps)
 	} else {
 		RenderVideo(w, r, ps)
-		// fmt.Fprintf(w, "hello, %s!\n", ps)
 	}
 }
 
 func RenderVideo(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-
 	var videoUrl = strings.TrimPrefix(ps.ByName("video"), "/")
-	fmt.Fprintf(w, "ASD XD, %s", videoUrl)
+
+	var videoHtml = fmt.Sprintf(`
+<html>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Discord Video Embedder</title>
+        <meta charset="UTF-8" />
+        <meta name="description" content="" />
+    </head>
+    <body style="background-color:#181a1b;">
+    <p>
+    <video
+        controls
+        autoplay
+        width="500px"
+        src="%s"
+    />
+    </p>
+    <p style="color:#d8d4cf">Original: <a style="color:#3391ff" href="%s">%s</a></p>
+    </body>
+</html>
+    `, videoUrl, videoUrl, videoUrl)
+
+	// fmt.Fprint(w, videoHtml)
+	fmt.Fprintf(w, "%s", videoHtml)
 }
 
 func main() {
